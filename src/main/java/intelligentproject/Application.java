663bd44bcc105680dde2a4cef65ec6deeb56ccc7
AdishwarS <Adishwar.Suresh@infinite.com>
2025-07-21 03:19:37 -0500

    Add src/main/java/intelligentproject/Application.java

---
+++ src/main/java/intelligentproject/Application.java
@@ -0,0 +1,11 @@
+package intelligentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentprojectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentprojectApplication.class, args);
+    }
+}
